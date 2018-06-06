b2f02ac29d0ac2095ccd05209621d3e5908aaf93
Mo Albazaz <malbazaz@mba2018.hbs.edu>
2018-06-06 16:45:21 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -5,3 +5,34 @@ $(document).ready(function(){
 // call functions here
 
 });
+
+function getIt(){
+  $('p').on('click', function(){
+  alert( "Hey!")
+  }
+  
+  )
+}
+
+function frameIt(){
+  $('img').addClass('tasty')
+  $('.tasty').on('load', function(){
+ document.getElementById('.tasty').style.border = "thick solid #ff0000" 
+    
+  })
+}
+
+function pressIt(){
+  $('document').on('keydown',function(key){
+    if(key.which==71){
+    alert('G key was pressed down')
+    }
+    }
+  )
+  }
+  
+function submitIt(){
+  $('form').on('submit',function(){
+alert("Your form is going to be submitted now.")
+  })
+}
